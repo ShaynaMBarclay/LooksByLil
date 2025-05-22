c07f16ff092af43b44569997e84d8943301a394e
ShaynaMBarclay <shaynambarclay@gmail.com>
2025-05-22 12:56:29 -0400

    added logo to topbar

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../styles/Topbar.css';
+import logoImg from '../assets/lilylogo.webp';
 
 function Topbar() {
   return (
-    <header className="topbar">
-           <div className="topbar-title">Looks by Lil</div>
+     <header className="topbar">
+      <img src={logoImg} alt="Looks by Lil Logo" className="topbar-logo" />
       <a
         href="https://www.looksbylil.com/"
         target="_blank"
