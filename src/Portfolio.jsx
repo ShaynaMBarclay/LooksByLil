2ee219da99bb05adb5afe5e9b50f9a43e95510a6
ShaynaMBarclay <shaynambarclay@gmail.com>
2025-05-22 17:25:27 -0400

    side bar update

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
+
 import { motion } from "framer-motion";
 import "./styles/Portfolio.css";
-import Spinner from "./components/Spinner";
+
 
 
 const portfolioImages = [
--- src/components/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-import razorImage from '../assets/razor.png';
-
-const Spinner = () => (
-  <div className="spinner">
-    <img src={razorImage} alt="Loading..." className="razor-spinner" />
-  </div>
-);
-
-export default Spinner;
